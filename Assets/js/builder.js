d453436e8225616db8127b663e11196513447f69
Henry Weismann <henry@weismannweb.com>
2020-11-16 22:02:03 -0500

    disable mautic global keyboard bindings
    This is related to this ticket https://github.com/Webmecanik/plugin-grapesjs-builder/issues/6

--- Assets/js/builder.js
+++ Assets/js/builder.js
@@ -74,6 +74,10 @@ Mautic.initGrapesJS = function (object) {
         'dynamicContentBtnLabel': Mautic.translate('grapesjsbuilder.dynamicContentBtnLabel'),
         'dynamicContentModalTitle': Mautic.translate('grapesjsbuilder.dynamicContentModalTitle'),
     };
+    
+    // disable mautic global shortcuts
+    Mousetrap.reset();
+
 
     // Redefine Keyboard shortcuts due to unbind won't works with multiple keys.
     let keymapsConf = {
