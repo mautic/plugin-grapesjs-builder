30fd045a3ac5da0676e9dd4a10e89b6dd95bec51
Patryk Gruszka <patryk.gruszka@comarch.pl>
2024-11-18 16:35:46 +0100

    feat: [DPMMA-2936] grapesjs alt ckeditor initialzizer

---
+++ Assets/library/js/plugins/grapesjs.ckeditor.js
@@ -0,0 +1,102 @@
+export default (editor, opts = {}) => {
+    const options = {
+        ...opts
+    };
+
+    let ckEditorInstance = null;
+
+    const SIMPLE_EDITING_TYPES = ['mj-button'];
+
+    editor.on('rte:enable', (view, gjsRte) => {
+        if (!isSimpleEditingEl(view.el)) {
+            initializeModal(gjsRte.el);
+            editor.RichTextEditor.hideToolbar();
+        }
+    });
+
+    function isSimpleEditingEl(el) {
+        return el && el.getAttribute && SIMPLE_EDITING_TYPES.includes(el.getAttribute('data-gjs-type'));
+    }
+
+    function initializeModal(el) {
+        const ckEditorElementId = generateId('ckeditor');
+        const modal = editor.Modal;
+        setupModalEvents(modal, ckEditorElementId);
+        openModal(modal, el, ckEditorElementId);
+    }
+
+    function setupModalEvents(modal, ckEditorElementId) {
+        modal.onceOpen(() => initCKEditor(ckEditorElementId));
+        modal.onceClose(() => destroyCKEditor());
+    }
+
+    function initCKEditor(elementId) {
+        if (typeof ClassicEditor === 'undefined') {
+            throw new Error('CKEDITOR instance not found');
+        }
+        ClassicEditor.create(document.getElementById(elementId), options)
+            .then(instance => {
+                ckEditorInstance = instance;
+            })
+            .catch(error => {
+                console.error('Error initializing CKEditor:', error);
+            });
+    }
+
+    function destroyCKEditor() {
+        if (ckEditorInstance) {
+            ckEditorInstance.destroy()
+                .catch(error => {
+                    console.error('Error destroying CKEditor instance:', error);
+                });
+        }
+    }
+
+    function openModal(modal, el, ckEditorElementId) {
+        modal.open({
+            title: 'Edit',
+            content: getModalContent(el, ckEditorElementId),
+            attributes: {
+                class: 'modalckeditor',
+                id: generateId('cke-modal')
+            }
+        });
+
+        setupModalButtons(el, modal);
+    }
+
+    function getModalContent(el, ckEditorElementId) {
+        return `
+            <style>
+                .modalckeditor .ck-editor .ck-editor__editable_inline {min-height:400px}
+                .modalckeditor .ck-editor .ck-content {background: #FFF;}
+                .modalckeditor .ck-editor .ck-content * {color: #000;}
+                .modalckeditor .gjs-btn-prim {margin: 10px 5px 5px 0;}
+            </style>
+            <div id="${ckEditorElementId}">${el.innerHTML}</div>
+            <button type="button" class="gjs-btn-prim" id="gjs-cke-save-btn">Save</button>
+            <button type="button" class="gjs-btn-prim" id="gjs-cke-close-btn">Close</button>
+        `;
+    }
+
+    function setupModalButtons(el, modal) {
+        document.getElementById('gjs-cke-save-btn').onclick = () => saveContent(el, modal);
+        document.getElementById('gjs-cke-close-btn').onclick = () => modal.close();
+    }
+
+    function saveContent(el, modal) {
+        if (ckEditorInstance) {
+            const content = ckEditorInstance.getData();
+            const selectedElement = editor.getSelected();
+            if (selectedElement.getEl().innerHTML !== content) {
+                selectedElement.components('');
+                selectedElement.set('content', content);
+            }
+        }
+        modal.close();
+    }
+
+    function generateId(prefix = 'el') {
+        return `${prefix}-${Math.random().toString(36).substring(2, 10)}`;
+    }
+};
